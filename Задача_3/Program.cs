da99e920235557c0973db9ab78948b2332d3fa5a
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-07-11 21:46:53 +0300

    Zadacha3

---
+++ Задача_3/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+
+Console.WriteLine("Введите число");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else 
+{
+    Console.WriteLine("Число не четное");
+}
