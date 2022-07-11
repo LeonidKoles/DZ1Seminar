8667f1c61293ba726660d833b1938808246f7299
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-07-11 21:37:06 +0300

    Zadacha2

---
+++ Задача_2/Program.cs
@@ -0,0 +1,17 @@
+﻿int a,b,c, max;
+
+Console.WriteLine("Введите три числа:");
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+max = a;
+
+if (b > max)
+    max = b;
+else
+{
+    if (c > max)
+    max = c;
+}   
+Console.WriteLine("{0} максимальное число", max);
