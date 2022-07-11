e548735d60046c86be171a1c3d4d1bcf731372d3
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-07-11 22:27:21 +0300

    Zadacha4

---
+++ Задача_4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n, start;
+
+Console.WriteLine("Введите число");
+n = Convert.ToInt32(Console.ReadLine());
+start = 2;
+
+while (start <= n)
+{  
+ 
+    Console.WriteLine(start + " ");
+    start = start + 2;
+}
+
